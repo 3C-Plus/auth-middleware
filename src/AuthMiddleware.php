ae8d719f17cecb83f1dc64dfd859cb100584a2bc
Gabriel Krysa <gabrielkrysa@gmail.com>
2025-03-17 20:07:41 -0300

    initial commit

---
+++ src/AuthMiddleware.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Dev3CPlus\Middleware;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Arr;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use GuzzleHttp\Client as GuzzleClient;
+use Predis\Client as RedisClient;
+
+class AuthMiddleware
+{
+    /**
+     * @throws \Exception
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $apiToken = $request->getQueryParams()['api_token']
+            ?? Arr::last(
+                explode(
+                    ' ',
+                    Arr::first($request->getHeader('Authorization'))
+                )
+            );
+
+        if (is_null($apiToken) || !env('URL_APPLICATION_API')) {
+            throw new \Exception('Unauthorized', 401);
+        }
+
+        $redisClient = new RedisClient([
+            'scheme' => 'tcp',
+            'host' => env('REDIS_CACHE_HOST'),
+            'port' => env('REDIS_CACHE_PORT'),
+        ]);
+
+        $cachedUserData = $this->searchUserInCache($redisClient, $apiToken);
+
+        if ($cachedUserData) {
+            $request = $request->withAttribute('auth_user', $cachedUserData);
+            return $handler->handle($request);
+        }
+
+        try {
+            $guzzleClient = new GuzzleClient();
+
+            $response = $guzzleClient->request(
+                'GET',
+                env('URL_APPLICATION_API') . '/me?include=company%2Cpermissions%2Cteams.instances', [
+                'headers' => [
+                    'Authorization' => "Bearer $apiToken",
+                    'Accept' => 'application/json,',
+                    'Referer' => 'https://app.3c.plus/',
+                ]
+            ]);
+
+            $data = json_decode($response->getBody()->getContents());
+
+            $redisClient->set($this->getCacheKey($apiToken), $data->data, 3600);
+        } catch (GuzzleException $e) {
+            throw new \Exception($e->getMessage(), 401);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage(), 500);
+        }
+
+        return $handler->handle($request);
+    }
+
+    private function searchUserInCache(RedisClient $redis, string $apiToken): ?string
+    {
+        return $redis->get($this->getCacheKey($apiToken));
+    }
+
+    private function getCacheKey(string $apiToken): string
+    {
+        return 'auth_user:' . md5($apiToken);
+    }
+}
